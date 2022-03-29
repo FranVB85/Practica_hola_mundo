d97bdd4080ca48ab9f9db97dd181871690437346
fvasqueb <fvasqueb@emeal.nttdata.com>
2022-03-29 14:43:01 +0200

    first commit

---
+++ src/main/java/com/everis/everisdesafiomnv_FJVB/App.java
@@ -0,0 +1,13 @@
+package com.everis.everisdesafiomnv_FJVB;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
